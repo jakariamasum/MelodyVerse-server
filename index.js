2bd7d6d584a3aa04887b48221f7b8c33017b3e80
jakariamasum <jmphy698@gmail.com>
2023-06-14 18:12:24 +0600

    final push

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
+        // await client.connect(); 
 
         const classesCollection = client.db("music-school").collection("classes");
         const studentsCollection = client.db("music-school").collection("students");
